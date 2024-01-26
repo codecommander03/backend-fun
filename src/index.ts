5476f283dd6fab21a98c52e188cd563997524389
Aditya <codecommander03@gmail.com>
2024-01-26 15:42:26 +0530

    db done

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -19,4 +20,10 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
     console.log('Server is listening on port 8080');
-})
+});
+
+const MONGO_URL = 'mongodb+srv://aditya:aditya@cluster0.7ynqduz.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
